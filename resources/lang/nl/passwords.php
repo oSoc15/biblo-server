8857ec2bb41dcf31c1c70919106b931b28980cf8
Wesley Vanbrabant <wesley@wesobi.be>
2015-07-14 16:37:56 +0200

    added dutch language to resources/lang

---
+++ resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten minstens 6 tekens lang zijn en gelijk zijn.',
+    'user' => "We kunnen geen gebruiker met dit email-adres vinden..",
+    'token' => 'dit wachtwoord reset token is ongeldig.',
+    'sent' => 'We hebben u uw passwoord reset link toegestuurd!',
+    'reset' => 'Uw wachtwoord is gereset!'
+
+];
