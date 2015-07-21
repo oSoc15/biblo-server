b4cf4ea6c52ee57771e43acdfd7dc4ebe5bd64e4
wesobi <vanbrabantwesley@MacBook-Pro-van-Wesley.local>
2015-07-21 13:42:39 +0200

    added functionality to drop

--- database/migrations/2015_07_21_113057_add_superuser_to_users_table.php
+++ database/migrations/2015_07_21_113057_add_superuser_to_users_table.php
@@ -25,7 +25,7 @@ class AddSuperuserToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('superuser');
         });
     }
 }
