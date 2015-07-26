9ab6b8f203c68b8c4003933af4ad2d191c9abc1d
wesobi <vanbrabantwesley@MacBook-Pro-van-Wesley.local>
2015-07-26 17:43:19 +0200

    merge conflict fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,13 +3,8 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<< HEAD
 use App\Illustration as Illustration;
 
-
-
-=======
->>>>>>> refs/remotes/origin/development
 class DatabaseSeeder extends Seeder
 {
     /**
